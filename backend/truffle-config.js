5a3674200c820eb635c8682e8397a5a0867b2a26
MoyosoreOdumosu <162631570+MoyosoreOdumosu@users.noreply.github.com>
2024-03-07 20:24:14 -0600

    truffle-config.js

--- backend/truffle-config.js
+++ backend/truffle-config.js
@@ -1,36 +1,16 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1", // Localhost (default: none)
-      port: 7545, // Standard Ethereum port (default: none)
-      network_id: "5777" // Any network (default: none)
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ganache port (default: none)
+      network_id: "*",       // Any network (default: none)
     },
-    sepolia: {
-      provider: () => new HDWalletProvider({
-        mnemonic: {
-          phrase: "story mobile mean sail wagon talk protect grief sadness neck letter off"
-        },
-        providerOrUrl: "https://sepolia.infura.io/v3/9bea25141b25496288fba11f6a06c1db"
-      }),
-      network_id: 11155111, // Sepolia's network ID
-      gas: 4000000, // Adjust the gas limit as per your requirements
-      gasPrice: 10000000000, // Set the gas price to an appropriate value
-      confirmations: 2, // Set the number of confirmations needed for a transaction
-      timeoutBlocks: 200, // Set the timeout for transactions
-      skipDryRun: true // Skip the dry run option
-    }
   },
+
+  // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.0", // Specify compiler version
-      settings: {
-        optimizer: {
-          enabled: true,
-          runs: 200
-        }
-      }
+      version: "0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
     }
   }
 };
